030b77e29b8d26283315a82efdbd15da41527308
joel-ortiz <joel.ortiz@schibsted.com>
2019-02-07 12:47:18 +0100

    Update webpack config for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,19 @@
+//const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+//const Dotenv = require('dotenv');
+
+//const env = Dotenv.config().parsed;
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  /*output: {
+    publicPath: `https://joeleduardo.github.io${env.PUBLIC_URL}/`
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'API_URL': JSON.stringify(env.API_URL),
+      'PUBLIC_URL': JSON.stringify(env.PUBLIC_URL)
+    })
+  ]*/
 });
